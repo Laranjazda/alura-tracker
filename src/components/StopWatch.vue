43e183d7eb11686a98730865c3e74c6057048dc3
laranjazda <carloseng.jr@gmail.com>
2022-04-19 01:02:08 -0300

    refactoring

---
+++ src/components/StopWatch.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <strong>{{ secondsTransform }}</strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "StopWatch",
+  props: {
+    secondsTime: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  computed: {
+    secondsTransform(): string {
+      return new Date(this.secondsTime * 1000).toISOString().substr(11, 8);
+    },
+  },
+});
+</script>
